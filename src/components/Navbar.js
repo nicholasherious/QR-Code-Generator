6e9f9edd65188cc8f7585126e17cc17aa4e57a5c
nicholasherious <nherious@gmail.com>
2020-04-17 02:28:59 -0700

    updated navbar link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,10 @@ function Navbar() {
   return (
     <div>
       <nav className="navbar navbar-light bg-light">
-        <a className="navbar-brand" href="#">
+        <a
+          className="navbar-brand"
+          href="https://github.com/nicholasherious/QR-Code-Generator"
+        >
           QR Generator
         </a>
       </nav>
